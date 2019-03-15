5520d0e66685b4fa09880f6052b61e55784d15d6
Elena Eigelyte <elena.eigelyte@gmail.com>
2019-03-15 13:07:26 +0000

    Change functions for the code efficiency

--- test_boggle.py
+++ test_boggle.py
@@ -67,7 +67,7 @@ class TestBoggle(unittest.TestCase):
             others = list(grid) # create a new list from the dictionary's keys
             others.remove(pos)
             self.assertListEqual(sorted(neighbours[pos]), sorted(others))
-            
+    
     def test_converting_a_path_to_a_word(self):
         """
         Ensure that paths can be converted to words
@@ -77,26 +77,30 @@ class TestBoggle(unittest.TestCase):
         twoLetterWord = boggle.path_to_word(grid, [(0, 0), (1, 1)])
         self.assertEqual(oneLetterWord, grid[(0, 0)])
         self.assertEqual(twoLetterWord, grid[(0, 0)] + grid[(1, 1)])
-        
+    
     def test_search_grid_for_words(self):
         """
-        Ensure that certain patterns can be found in a path_to_word
+        Ensure that certain patterns can be found in a `path_to_word`
         """
         grid = {(0, 0): 'A', (0, 1): 'B', (1, 0): 'C', (1, 1): 'D'}
         twoLetterWord = 'AB'
         threeLetterWord = 'ABC'
         notThereWord = 'EEE'
-        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
         
+        fullwords = [twoLetterWord, threeLetterWord, notThereWord]
+        stems = ['A', 'AB', 'E', 'EE']
+        dictionary = fullwords, stems
+
         foundWords = boggle.search(grid, dictionary)
-        
+
         self.assertTrue(twoLetterWord in foundWords)
         self.assertTrue(threeLetterWord in foundWords)
         self.assertTrue(notThereWord not in foundWords)
-        
+    
     def test_load_dictionary(self):
         """
-        Test that a 'get_dictionary' function returns a dictionary that has a lenght greater than 0
+        Test that the `get_dictionary` function returns a dictionary
+        that has a length greater than 0
         """
         dictionary = boggle.get_dictionary('words.txt')
         self.assertGreater(len(dictionary), 0)
